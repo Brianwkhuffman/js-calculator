cbfdca44b5d23d64f5ad4108e50a913e5ed8c5c2
Briii <brianwkhuffman@gmail.com>
2019-06-19 21:33:14 -1000

    FINISHED!

--- calculator.js
+++ calculator.js
@@ -5,14 +5,66 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
-
+function calculatorModule(){
+    var memory = 0;
+    var total = 0;
+    var calculator = {
+        load: function(x){
+            if (typeof x === 'number'){
+                return total = x;
+            }else{
+                throw error;
+            }
+        },
+        getTotal: function(){
+            return total + memory;
+        },
+        add: function(x){
+            if (typeof x === 'number'){
+                return total += x;
+            }else{
+                throw error;
+            }
+        },
+        subtract: function(x){
+            if (typeof x === 'number'){
+                return total -= x;
+            }else{
+                throw error;
+            }
+        },
+        multiply: function(x){
+            if (typeof x === 'number'){
+                return total *= x;
+            }else{
+                throw error;
+            }
+        },
+        divide: function(x){
+            if (typeof x === 'number'){
+                return total /= x;
+            }else{
+                throw error;
+            }
+        },
+        recallMemory: function(){
+            return memory;
+        },
+        saveMemory: function(){
+            memory = total;
+        },
+        clearMemory: function(){
+            memory = 0;
+        }
+    }
+    return calculator;
+}
 
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
-
 
   /**
    * Return the value of `total`
